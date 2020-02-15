630177320655f895215694e5f40d26709bdec504
TheVictorHugo <victorq32@gmail.com>
2020-02-15 00:56:24 -0300

    Mask and apply to the image
    A circular mask was applied anda applied to the image

--- code.py
+++ code.py
@@ -132,3 +132,33 @@
 # img_rotacionada = cv2.warpAffine(img, M, (lar, alt))
 # cv2.imshow("Imagem rotacionada em 45 graus", img_rotacionada)
 # cv2.waitKey(0)
+
+#########################################################
+# Mascara
+# import cv2
+# import numpy as np
+#
+# img = cv2.imread('img/lenna2.jpg')
+# cv2.imshow("Oraginal", img)
+# mascara = np.zeros(img.shape[:2], dtype = "uint8")
+# (cX,cY) = (img.shape[1] //2, img.shape[0] //2)
+# cv2.circle(mascara, (cX, cY), 100, 255, -1)
+# img_com_mascara = cv2.bitwise_and(img, img, mask = mascara)
+# cv2.imshow("Máscara aplicada à imagem", img_com_mascara)
+# cv2.waitKey(0)
+
+#########################################################
+# Mascara
+# import cv2
+# import numpy as np
+#
+# img = cv2.imread('img/lenna2.jpg')
+# cv2.imshow("Original",img)
+# mascara = np.zeros(img.shape[:2],dtype = "uint8")
+# (cX,cY) = (img.shape[1] //2,img.shape[0] //2)
+# cv2.circle(mascara,(cX,cY),180,255,70)
+# cv2.circle(mascara, (cX,cY),70,255,-1)
+# cv2.imshow("Mácara", mascara)
+# img_com_mascara = cv2.bitwise_and(img, img, mask = mascara)
+# cv2.imshow("Máscara aplicada à imagem", img_com_mascara)
+# cv2.waitKey(0)
